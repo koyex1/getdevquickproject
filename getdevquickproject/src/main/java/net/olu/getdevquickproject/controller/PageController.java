d531b8abc62faca119476daf56efafbdc6927470
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2020-07-21 02:36:27 +0100

    jsp, front controller ready

---
+++ getdevquickproject/src/main/java/net/olu/getdevquickproject/controller/PageController.java
@@ -0,0 +1,18 @@
+package net.olu.getdevquickproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","/home","index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("storename","Dominoe Books");
+		return mv;
+	}
+	
+	
+}
